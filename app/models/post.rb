3ff0c46f4fe053ae7a66304b597561ec1a60a845
wbdana <william.b.dana@gmail.com>
2017-06-23 13:30:41 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
+  before_validation :make_title_case
 
   private
 
@@ -15,4 +16,5 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
 end
